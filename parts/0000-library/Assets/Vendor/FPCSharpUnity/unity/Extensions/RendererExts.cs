d62afa54861b8cd9e19d246912796538cfe04688
Evaldas Ciakas <evaldas757@gmail.com>
2022-01-26 13:18:54 +0200

    Fix runtime error

--- parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Extensions/RendererExts.cs
+++ parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Extensions/RendererExts.cs
@@ -1,15 +1,16 @@
 using System;
 using GenerationAttributes;
 using UnityEngine;
+using static System.Reflection.BindingFlags;
 
 namespace FPCSharpUnity.unity.Extensions {
   public static class RendererExts {
     public delegate int RendererGetMaterialCount(Renderer self);
     public static readonly RendererGetMaterialCount GetMaterialCount = 
-      (RendererGetMaterialCount) System.Delegate.CreateDelegate(
+      (RendererGetMaterialCount) Delegate.CreateDelegate(
         type: typeof(RendererGetMaterialCount), firstArgument: null, 
         // ReSharper disable once AssignNullToNotNullAttribute
-        method: typeof(Renderer).GetMethod("GetMaterialCount", System.Reflection.BindingFlags.NonPublic)
+        method: typeof(Renderer).GetMethod("GetMaterialCount", NonPublic | Instance)
       );
 
     public static int GetMaterialCountReflected(this Renderer r) => GetMaterialCount(r);
