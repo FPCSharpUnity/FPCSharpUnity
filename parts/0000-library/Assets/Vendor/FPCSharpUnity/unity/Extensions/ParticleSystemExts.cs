43f520bfd68ed71378323c1d8e707d1742ac42ea
OpTick <pauliussuku@gmail.com>
2022-02-22 13:33:16 +0200

    particle system exts

--- parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Extensions/ParticleSystemExts.cs
+++ parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Extensions/ParticleSystemExts.cs
@@ -1,4 +1,6 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using FPCSharpUnity.core.exts;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace FPCSharpUnity.unity.Extensions {
@@ -34,6 +36,37 @@ namespace FPCSharpUnity.unity.Extensions {
     public static void setStartColor(this ParticleSystem system, Color color) {
       var main = system.main;
       main.startColor = color;
+    }
+
+    /// <summary>Actions for controlling <see cref="ParticleSystem"/>s 'Play' and 'Stop'.</summary>
+    public enum Control : byte {
+      /// <summary>Plays particle system with children, <see cref="ParticleSystem.Play()"/>.</summary>
+      Play, 
+      /// <summary>Stops particle system with children, <see cref="ParticleSystem.Stop()"/>.</summary>
+      Stop
+    }
+
+    public static Control toParticleSystemControl(this bool enabled) => enabled ? Control.Play : Control.Stop;
+
+    /// <summary>
+    /// Use this when you want to avoid multiple <see cref="ParticleSystem.Play()"/> or <see cref="ParticleSystem.Stop()"/>
+    /// calls on update, for this '<see cref="particleSystem"/>'.
+    /// </summary>
+    public static void handleOnUpdate(this ParticleSystem particleSystem, Control control) {
+      switch (control) {
+        case Control.Play:
+          if (!particleSystem.isPlaying) {
+            particleSystem.Play();
+          }
+          break;
+        case Control.Stop:
+          if (particleSystem.isPlaying) {
+            particleSystem.Stop();
+          }
+          break;
+        default:
+          throw control.argumentOutOfRange();
+      }
     }
   }
 }
