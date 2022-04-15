00e1e873d49de7f83d7c879de68ef6d70df3e60c
Arturas Slajus <x11@arturaz.net>
2022-04-15 13:39:45 +0300

    Comment.

--- parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/unity_serialization/StrUnity.cs
+++ parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/unity_serialization/StrUnity.cs
@@ -1,6 +1,10 @@
-﻿using UnityEngine;
+﻿using FPCSharpUnity.core.typeclasses;
+using UnityEngine;
 
 namespace FPCSharpUnity.unity.unity_serialization {
+  /// <summary>
+  /// <see cref="Str"/> instances/functions for Unity types.
+  /// </summary>
   public class StrUnity {
     public static string s(Hash128 v) => v.ToString();
   }
