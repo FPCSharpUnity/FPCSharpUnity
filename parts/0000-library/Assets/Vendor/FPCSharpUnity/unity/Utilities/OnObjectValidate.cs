45a684a3229199901f6028a55861dafd056dd64a
Arturas Slajus <x11@arturaz.net>
2022-04-21 16:35:45 +0300

    PrepareForBuild.cs -> OnObjectValidate.cs

--- parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Utilities/OnObjectValidate.cs
+++ parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Utilities/OnObjectValidate.cs
@@ -3,20 +3,23 @@ using FPCSharpUnity.unity.Data;
 using UnityEngine;
 
 namespace FPCSharpUnity.unity.Utilities {
+  /// <summary>
+  /// If a type implements this interface, the Unity object validator will invoke <see cref="onObjectValidate"/> when
+  /// validating the object.
+  /// </summary>
   public interface OnObjectValidate {
     /// <summary>
     /// Is validation of this object thread safe? If false is returned the validation will be scheduled to be executed
     /// on Unity main thread. 
     /// </summary>
     bool onObjectValidateIsThreadSafe { get; }
+    
     /// <summary>
-    /// onObjectValidate is called when ObjectValidator
-    /// begins to validate the object implementing this interface.
-    /// <param name="containingComponent">
-    /// Can be used, for example, to mark any field updates
-    /// during build time using .recordEditorChanges
-    /// </param>
+    /// This method is called when `ObjectValidator` begins to validate the object implementing this interface.
     /// </summary>
+    /// <param name="containingComponent">
+    /// Can be used, for example, to mark any field updates during build time using `.recordEditorChanges`.
+    /// </param>
     IEnumerable<ErrorMsg> onObjectValidate(Object containingComponent);
   }
 }
