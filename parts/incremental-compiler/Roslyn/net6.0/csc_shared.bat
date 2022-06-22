b30d7d390dfa39824d8fb45db2a929595d36b829
Evaldas Ciakas <evaldas757@gmail.com>
2022-06-22 23:35:29 +0300

    Updated compiler

--- parts/incremental-compiler/Roslyn/net6.0/csc_shared.bat
+++
@@ -1,3 +0,0 @@
-REM Used by .csproj files to make compilation faster
-
-dotnet "%~dp0\csc.dll" -shared %*
--- parts/incremental-compiler/Roslyn/net6.0/csc_shared.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Used by .csproj files to make compilation faster
-
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
-
-dotnet "$SCRIPT_DIR/csc.dll" -shared "$@"
