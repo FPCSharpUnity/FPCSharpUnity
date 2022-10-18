3ff1b2dbc7558b69b926cf8b43302ceabcd23f1c
pc1 <paulius.cib@gmail.com>
2022-10-18 13:06:39 +0300

    SerializableDictionaryBase added selector for enums, moved GenericOdinSelector to fpLib

--- parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/unity_serialization/SerializableDictionaryBase.cs
+++ parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/unity_serialization/SerializableDictionaryBase.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using FPCSharpUnity.core.exts;
 using FPCSharpUnity.unity.Data;
 using FPCSharpUnity.unity.Utilities;
 using GenerationAttributes;
 using JetBrains.Annotations;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using Vendor.FPCSharpUnity.unity.Utilities;
 using Object = UnityEngine.Object;
 
 namespace FPCSharpUnity.unity.unity_serialization;
@@ -15,9 +17,28 @@ namespace FPCSharpUnity.unity.unity_serialization;
 public abstract partial class SerializableDictionaryBase<A, B> : OnObjectValidate {
 #pragma warning disable 649
   // ReSharper disable NotNullMemberIsNotInitialized
-  [SerializeField, NotNull, TableList, OnValueChanged(nameof(valueChanged))] protected Pair[] _keyValuePairs = {};
+  [
+    SerializeField, NotNull, TableList, OnValueChanged(nameof(valueChanged)),
+    ListDrawerSettings(CustomAddFunction = nameof(selector))
+  ] protected Pair[] _keyValuePairs = {};
   // ReSharper restore NotNullMemberIsNotInitialized
 #pragma warning restore 649
+
+  /// <summary> This doesn't work if inspector draws array as table. So we show it above table as extra button. </summary>
+  [Button, ShowIf(nameof(showAddButton)), PropertyOrder(-1)] void addKey() => selector();
+  bool showAddButton => typeof(A).IsEnum;
+
+  void selector() {
+    if (typeof(A).IsEnum) {
+      GenericOdinSelector.showPopup(
+        ((A[])Enum.GetValues(typeof(A))).Where(e => !_keyValuePairs.Any(kvp => kvp.key.Equals(e))),
+        onSelect: e => _keyValuePairs = _keyValuePairs.addOne(new Pair(e, default))
+      );
+    } 
+    else {
+      _keyValuePairs = _keyValuePairs.addOne(new Pair(default, default));
+    }
+  }
 
   protected abstract void valueChanged();
 
