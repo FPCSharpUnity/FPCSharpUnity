bc759b86f07bc1f26ca5c0502b784c7adf97456e
Boris Proshin <b.proshin@game-insight.com>
2022-11-30 00:21:24 +0300

    New Color tag and sizetag

--- parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Extensions/StringExtsU.cs
+++ parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Extensions/StringExtsU.cs
@@ -10,4 +10,20 @@ public static class StringExtsU {
   /// </summary>
   public static string wrapInColorTag(this string content, Color color) =>
     $"<color=#{ColorUtility.ToHtmlStringRGBA(color)}>{content}</color>";
+  
+  /// <summary>
+  /// Returns the syntax for Unity text handlers to color the <see cref="content"/>.
+  /// <para/>
+  /// See https://docs.unity3d.com/Packages/com.unity.ugui@1.0/manual/StyledText.html#supported-tags for more info.
+  /// </summary>
+  public static string wrapInColorTag(this string content, string htmlColor) =>
+    $"<color={htmlColor}>{content}</color>";
+
+  /// <summary>
+  /// Returns the syntax for Unity text handlers to size the <see cref="content"/>.
+  /// <para/>
+  /// See https://docs.unity3d.com/Packages/com.unity.ugui@1.0/manual/StyledText.html#supported-tags for more info.
+  /// </summary>
+  public static string wrapInSizeTag(this string content, string size) =>
+    $"<size={size}>{content}</size>";
 }
