d33d408fb266a6a74be44215e224dec490daeabc
Evaldas Ciakas <evaldas757@gmail.com>
2022-11-29 20:53:37 +0200

    Refactored UIClickForwarder

--- parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Components/Forwarders/UIClickForwarder.cs
+++ parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Components/Forwarders/UIClickForwarder.cs
@@ -1,22 +1,16 @@
-﻿using System;
-using FPCSharpUnity.unity.Data;
-using FPCSharpUnity.unity.Functional;
+﻿using FPCSharpUnity.unity.Functional;
 using FPCSharpUnity.core.reactive;
-
-using JetBrains.Annotations;
 using FPCSharpUnity.core.functional;
+using FPCSharpUnity.core.macros;
 using UnityEngine.EventSystems;
 
 namespace FPCSharpUnity.unity.Components {
-  public class UIClickForwarder : UIBehaviour, IPointerClickHandler {
-    readonly Subject<Unit> _onClick = new();
-    public IRxObservable<Unit> onClick => _onClick;
+  public partial class UIClickForwarder : UIBehaviour, IPointerClickHandler {
+    [PublicReadOnlyAccessor] readonly Subject<Unit> _onClick = new();
 
     public void OnPointerClick(PointerEventData eventData) {
       if (eventData.button == PointerEventData.InputButton.Left && IsActive())
         _onClick.push(F.unit);
     }
   }
-
-  [Serializable, PublicAPI] public class UIClickForwarderPrefab : TagPrefab<UIClickForwarder> {}
 }
