88d4e9ca5976c2a760b36f0e8e55220ac5e6607d
Arturas Slajus <x11@arturaz.net>
2023-02-01 16:49:03 +0200

    Make `SceneBuildIndex` implement `IStr`

--- parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Data/scenes/SceneBuildIndex.cs
+++ parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Data/scenes/SceneBuildIndex.cs
@@ -1,11 +1,16 @@
 using FPCSharpUnity.core.macros;
+using FPCSharpUnity.core.typeclasses;
 using FPCSharpUnity.unity.Extensions;
 using GenerationAttributes;
 
 namespace FPCSharpUnity.unity.Data.scenes {
   /// <summary>Index of a scene in the build settings. Obtained via <see cref="SceneExts.sceneBuildIndex"/>.</summary>
   [Record(ConstructorFlags.Constructor), NewTypeImplicitTo]
-  public readonly partial struct SceneBuildIndex { public readonly int index; }
+  public readonly partial struct SceneBuildIndex : IStr { 
+    public readonly int index;
+
+    public string asString() => index.ToString();
+  }
 
   /// <summary>Failure case for <see cref="SceneExts.sceneBuildIndex"/>.</summary>
   public enum SceneBuildIndexError {
