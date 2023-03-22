a4bc2143aad3c801ca81bc4fa94ee9326319b65a
Evaldas Ciakas <evaldas757@gmail.com>
2023-03-22 16:01:11 +0200

    Minor refactoring

--- parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Data/Size.cs
+++ parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Data/Size.cs
@@ -1,34 +1,12 @@
-﻿using System;
+﻿using GenerationAttributes;
+using UnityEngine;
 
-namespace FPCSharpUnity.unity.Data {
-  public struct Size : IEquatable<Size> {
-    public readonly int width, height;
+namespace FPCSharpUnity.unity.Data; 
 
-    public Size(int width, int height) {
-      this.width = width;
-      this.height = height;
-    }
+[Record(GenerateToString = false)] public readonly partial struct Size {
+  public readonly int width, height;
 
-    #region Equality
-
-    public bool Equals(Size other) {
-      return width == other.width && height == other.height;
-    }
-
-    public override bool Equals(object obj) {
-      if (ReferenceEquals(null, obj)) return false;
-      return obj is Size && Equals((Size) obj);
-    }
-
-    public override int GetHashCode() {
-      unchecked { return (width * 397) ^ height; }
-    }
-
-    public static bool operator ==(Size left, Size right) { return left.Equals(right); }
-    public static bool operator !=(Size left, Size right) { return !left.Equals(right); }
-
-    #endregion
-
-    public override string ToString() { return $"{nameof(Size)}[{width}x{height}]"; }
-  }
+  public override string ToString() { return $"{nameof(Size)}[{width}x{height}]"; }
+  
+  public Vector2Int vector2Int => new(width, height);
 }
