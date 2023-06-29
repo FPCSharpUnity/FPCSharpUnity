14c9334ae86e5eb442dd152e57cb681bfeabee44
Arturas Slajus <x11@arturaz.net>
2023-06-29 10:56:47 +0300

    ConfigU.colorRBGAParser

--- parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Configuration/ConfigU.cs
+++ parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Configuration/ConfigU.cs
@@ -19,5 +19,16 @@ namespace FPCSharpUnity.unity.Configuration {
     
     public static readonly Parser<JsonValue, Color> colorParser =
       stringParser.flatMap(s => ColorUtility.TryParseHtmlString(s, out var c) ? Some.a(c) : None._);
+    
+    /// <summary>
+    /// Parses a <see cref="Color"/> from {"r": 0.0, "g": 0.0, "b": 0.0, "a": 0.0} JSON.
+    /// </summary>
+    public static readonly Parser<JsonValue, Color> colorRGBAParser =
+      configParser.flatMapTry((_, cfg) => new Color(
+        r: cfg.getFloat("r"),
+        g: cfg.getFloat("g"),
+        b: cfg.getFloat("b"),
+        a: cfg.getFloat("a")
+      ));
   }
 }
