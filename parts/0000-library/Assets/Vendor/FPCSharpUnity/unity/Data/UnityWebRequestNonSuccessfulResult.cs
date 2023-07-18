1ff486a58db1603548d649a9fde5e2be62180244
Arturas Slajus <x11@arturaz.net>
2023-07-18 13:49:23 +0300

    Bunch of improvements regarding UnityWebRequest handling.

--- parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Data/UnityWebRequestNonSuccessfulResult.cs
+++ parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Data/UnityWebRequestNonSuccessfulResult.cs
@@ -1,34 +1,38 @@
-using System;
+using ExhaustiveMatching;
 using FPCSharpUnity.core.functional;
 using UnityEngine.Networking;
 
-namespace FPCSharpUnity.unity.Data {
-  /// <summary>
-  /// As <see cref="UnityWebRequest.Result"/>, however does not include the <see cref="UnityWebRequest.Result.Success"/>
-  /// case.
-  /// </summary>
-  public enum UnityWebRequestNonSuccessfulResult {
-    /// <inheritdoc cref="UnityWebRequest.Result.InProgress"/>
-    InProgress = UnityWebRequest.Result.InProgress,
-    /// <inheritdoc cref="UnityWebRequest.Result.ConnectionError"/>
-    ConnectionError = UnityWebRequest.Result.ConnectionError,
-    /// <inheritdoc cref="UnityWebRequest.Result.ProtocolError"/>
-    ProtocolError = UnityWebRequest.Result.ProtocolError,
-    /// <inheritdoc cref="UnityWebRequest.Result.DataProcessingError"/>
-    DataProcessingError = UnityWebRequest.Result.DataProcessingError
-  }
+namespace FPCSharpUnity.unity.Data; 
 
-  public static class UnityWebRequestNonSuccessfulResult_ {
-    public static Option<UnityWebRequestNonSuccessfulResult> fromResult(UnityWebRequest.Result r) => r switch {
+/// <summary>
+/// As <see cref="UnityWebRequest.Result"/>, however does not include the <see cref="UnityWebRequest.Result.Success"/>
+/// case.
+/// </summary>
+public enum UnityWebRequestNonSuccessfulResult {
+  /// <inheritdoc cref="UnityWebRequest.Result.InProgress"/>
+  InProgress = UnityWebRequest.Result.InProgress,
+    
+  /// <inheritdoc cref="UnityWebRequest.Result.ConnectionError"/>
+  ConnectionError = UnityWebRequest.Result.ConnectionError,
+    
+  /// <inheritdoc cref="UnityWebRequest.Result.ProtocolError"/>
+  ProtocolError = UnityWebRequest.Result.ProtocolError,
+    
+  /// <inheritdoc cref="UnityWebRequest.Result.DataProcessingError"/>
+  DataProcessingError = UnityWebRequest.Result.DataProcessingError
+}
+
+public static class UnityWebRequestNonSuccessfulResultExts {
+  /// <summary>
+  /// Returns `Some` if the <see cref="UnityWebRequest.Result"/> is not <see cref="UnityWebRequest.Result.Success"/>.
+  /// </summary>
+  public static Option<UnityWebRequestNonSuccessfulResult> toNonSuccessfulResult(this UnityWebRequest.Result r) =>
+    r switch {
       UnityWebRequest.Result.Success => None._,
       UnityWebRequest.Result.InProgress => Some.a(UnityWebRequestNonSuccessfulResult.InProgress),
       UnityWebRequest.Result.ConnectionError => Some.a(UnityWebRequestNonSuccessfulResult.ConnectionError),
       UnityWebRequest.Result.ProtocolError => Some.a(UnityWebRequestNonSuccessfulResult.ProtocolError),
       UnityWebRequest.Result.DataProcessingError => Some.a(UnityWebRequestNonSuccessfulResult.DataProcessingError),
-      _ => throw new ArgumentOutOfRangeException(nameof(r), r, null)
+      _ => throw ExhaustiveMatch.Failed(r)
     };
-
-    public static Option<UnityWebRequestNonSuccessfulResult> toNonSuccessfulResult(this UnityWebRequest.Result r) =>
-      fromResult(r);
-  }
 }
