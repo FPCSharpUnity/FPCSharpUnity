8d7dcb51ec531b64a60b8e074af49a89630e7573
Arturas Slajus <x11@arturaz.net>
2023-08-10 10:23:41 +0300

    Improvements and fixes for `GenericOdinSelector`
    - Fix positioning of an opened popup so it would be in screen when 'Preferences/Ui scaling' is not 100%.
    - Ability to set the initial search term on opening the popup.

--- parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Utilities/GenericOdinSelector.Editor.cs
+++ parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Utilities/GenericOdinSelector.Editor.cs
@@ -1,8 +1,10 @@
 #if UNITY_EDITOR
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using FPCSharpUnity.core.exts;
 using Sirenix.OdinInspector.Editor;
+using UnityEditor;
 using UnityEngine;
 
 namespace Vendor.FPCSharpUnity.unity.Utilities;
@@ -14,17 +16,27 @@ public class GenericOdinSelector<A> : OdinSelector<A> {
   readonly IEnumerable<A> valuesToPickFrom;
   readonly Func<A, string> getName;
   readonly Func<A, Sprite> getIcon;
+  readonly string initialSearchTerm;
 
   /// <param name="valuesToPickFrom"></param>
   /// <param name="getName">Will use ToString() by default.</param>
   /// <param name="getIcon">Won't show an icon be default.</param>
   GenericOdinSelector(
-    IEnumerable<A> valuesToPickFrom, Func<A, string> getName = null, Func<A, Sprite> getIcon = null
+    IEnumerable<A> valuesToPickFrom, Func<A, string> getName = null, Func<A, Sprite> getIcon = null,
+    string initialSearchTerm = ""
   ) {
     this.valuesToPickFrom = valuesToPickFrom;
     this.getName = getName ?? (a => a.ToString());
     this.getIcon = getIcon;
+    this.initialSearchTerm = initialSearchTerm;
   }
+
+  /* doesn't work!
+   protected override float DefaultWindowWidth() {
+    var resolution = Screen.currentResolution;
+    var hasLongNames = valuesToPickFrom.Max(v => getName(v).Split('/').Max(str => str.Length)) > 50;
+    return hasLongNames ? resolution.width / 2f : 350f;
+  }*/
 
   /// <param name="valuesToPickFrom"></param>
   /// <param name="onSelect">Callback when user selects an item and selection popup closes.</param>
@@ -32,7 +44,7 @@ public class GenericOdinSelector<A> : OdinSelector<A> {
   /// <param name="getIcon">Won't show an icon be default.</param>
   public static void showPopup(
     IEnumerable<A> valuesToPickFrom, Action<A> onSelect, Func<A, string> getName = null, 
-    Func<A, Sprite> getIcon = null
+    Func<A, Sprite> getIcon = null, string initialSearchTerm = ""
   ) {
     var selector = new GenericOdinSelector<A>(valuesToPickFrom, getIcon: getIcon, getName: getName);
     selector.SelectionConfirmed += selection => {
@@ -40,7 +52,35 @@ public class GenericOdinSelector<A> : OdinSelector<A> {
         onSelect(selectedValue);
       }}
     };
-    selector.ShowInPopup();
+    
+    var window = selector.ShowInPopup();
+
+    // Window.position is broken when 'Preferences/Ui scaling' is not 100%.
+    //
+    // For example if we try to put window at x=100 and y=100 with 150% scaling, the window gets placed at x=150 and
+    // y=150 instead.
+    // 
+    // We can fix this by dividing position by UI scale factor.
+    var pixelsPerUnit = EditorGUIUtility.pixelsPerPoint;
+    
+    var resolution = Screen.currentResolution;
+    var displayWidth = resolution.width;
+    var displayHeight = resolution.height;
+
+    selector.SelectionTree.Config.SearchTerm = initialSearchTerm;
+
+    EditorApplication.update += adjustPosition;
+
+    void adjustPosition() {
+      if (window) {
+        var pos = window.position;
+        var windowX = Mathf.Clamp(pos.x * pixelsPerUnit, 0, displayWidth - pos.width * pixelsPerUnit) / pixelsPerUnit;
+        var windowY = Mathf.Clamp(pos.y * pixelsPerUnit, 0, displayHeight - pos.height * pixelsPerUnit) / pixelsPerUnit;
+        //Debug.LogError($"{resolution.echo()}, {pos}, {windowX.echo()}, {windowY.echo()}, {pixelsPerUnit.echo()}");
+        window.position = new Rect(windowX, windowY, pos.width, pos.height);
+      }
+      else EditorApplication.update -= adjustPosition;
+    }
   }
 
   protected override void BuildSelectionTree(OdinMenuTree tree) {
@@ -52,6 +92,7 @@ public class GenericOdinSelector<A> : OdinSelector<A> {
       if (getIcon != null) tree.Add(getName(a), a, getIcon(a));
       else tree.Add(getName(a), a);
     }
+    SelectionTree.Config.SearchTerm = initialSearchTerm;
   }
 }
 
@@ -59,10 +100,10 @@ public static class GenericOdinSelector {
   /// <inheritdoc cref="GenericOdinSelector{A}.showPopup"/>
   public static void showPopup<A>(
     IEnumerable<A> valuesToPickFrom, Action<A> onSelect, Func<A, string> getName = null,
-    Func<A, Sprite> getIcon = null
+    Func<A, Sprite> getIcon = null, string initialSearchTerm = ""
   ) =>
     GenericOdinSelector<A>.showPopup(valuesToPickFrom: valuesToPickFrom, onSelect: onSelect, getName: getName,
-      getIcon: getIcon
+      getIcon: getIcon, initialSearchTerm: initialSearchTerm
     );
 }
 #endif
