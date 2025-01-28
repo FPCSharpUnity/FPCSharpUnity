8391da4cb2a416532ed879aca7e7e7f6c0db5e37
Evaldas Ciakas <evaldas757@gmail.com>
2025-01-28 18:40:32 +0200

    PointerDownUp improvements

--- parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Components/ui/PointerDownUp.cs
+++ parts/0000-library/Assets/Vendor/FPCSharpUnity/unity/Components/ui/PointerDownUp.cs
@@ -1,46 +1,61 @@
 ﻿using System.Collections.Generic;
+using FPCSharpUnity.core.functional;
+using FPCSharpUnity.core.reactive;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-namespace FPCSharpUnity.unity.Components.ui {
-  public abstract class PointerDownUp : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
-    /// <summary>
-    /// We need a list here, because:
-    /// * If we made several touches and then released some of them, we need to
-    ///   set isHeldDown to the last touch position we still have. Therefore we
-    ///   cannot use a counter.
-    /// * we can release touches in different order than we pressed. Therefore
-    ///   we cannot use a stack.
-    ///
-    /// The amount of elements should be small (amount of simultaneous touches
-    /// recognized by a device, which is usually less than 10), therefore the
-    /// performance gains from switching to other data structure would be
-    /// negligable here.
-    /// </summary>
-    protected readonly List<PointerEventData> pointerData = new List<PointerEventData>();
+namespace FPCSharpUnity.unity.Components.ui;
 
-    public void OnPointerDown(PointerEventData eventData) {
-      // Theoretically for every pointer down there should be a pointer up.
-      //
-      // However sometimes unity dispatches two pointer downs and one pointer up.
-      // So far we only noticed this behaviour in unity editor at low frame rates.
-      //
-      // Our hypothesis is that user clicks and releases the mouse within single frame.
-      //
-      // So the workaround is to double-check if this event has been dispatched by unity
-      // before and ignore it if has.
-      if (pointerData.Contains(eventData)) return;
+public abstract class PointerDownUp : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler {
+  /// <summary>
+  /// We need a list here, because:
+  /// * If we made several touches and then released some of them, we need to
+  ///   set isHeldDown to the last touch position we still have. Therefore we
+  ///   cannot use a counter.
+  /// * we can release touches in different order than we pressed. Therefore
+  ///   we cannot use a stack.
+  ///
+  /// The amount of elements should be small (amount of simultaneous touches
+  /// recognized by a device, which is usually less than 10), therefore the
+  /// performance gains from switching to other data structure would be
+  /// negligable here.
+  /// </summary>
+  protected readonly List<PointerEventData> pointerData = new();
 
-      pointerData.Add(eventData);
-      onPointerDown(eventData);
+  public readonly Subject<Unit> onExit = new();
+
+  public void OnPointerDown(PointerEventData eventData) {
+    // Theoretically for every pointer down there should be a pointer up.
+    //
+    // However sometimes unity dispatches two pointer downs and one pointer up.
+    // So far we only noticed this behaviour in unity editor at low frame rates.
+    //
+    // Our hypothesis is that user clicks and releases the mouse within single frame.
+    //
+    // So the workaround is to double-check if this event has been dispatched by unity
+    // before and ignore it if has.
+    if (pointerData.Contains(eventData)) return;
+
+    pointerData.Add(eventData);
+    onPointerDown(eventData);
+  }
+
+  public void OnPointerUp(PointerEventData eventData) {
+    pointerData.Remove(eventData);
+    onPointerUp(eventData);
+  }
+
+  protected abstract void onPointerDown(PointerEventData eventData);
+  protected abstract void onPointerUp(PointerEventData eventData);
+    
+  public void OnPointerExit(PointerEventData eventData) {
+    onExit.push(Unit._);
+  }
+
+  public void clearActivePointers() {
+    foreach (var pointer in pointerData) {
+      pointer.eligibleForClick = false;
     }
-
-    public void OnPointerUp(PointerEventData eventData) {
-      pointerData.Remove(eventData);
-      onPointerUp(eventData);
-    }
-
-    protected abstract void onPointerDown(PointerEventData eventData);
-    protected abstract void onPointerUp(PointerEventData eventData);
+    pointerData.Clear();
   }
 }
